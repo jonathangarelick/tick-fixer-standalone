4945d1400c31eabd1c00cec6edbe87bd8c09e111
jonathangarelick <41495718+jonathangarelick@users.noreply.github.com>
2024-04-30 16:06:53 -0700

    Improve pinging and cleanup code

--- TickFixer/PingManager.swift
+++ TickFixer/PingManager.swift
@@ -1,64 +1,79 @@
+import AppKit
 import Foundation
 import OSLog
 import SystemConfiguration
 
 class PingManager: ObservableObject {
-    @Published var defaultGateway: String?
-    
-    private var pingProcess: Process?
-    
+    @Published var running: Bool = false
+
+    var process: Process = Process()
+    var failureCount: Int = 0
+
     init() {
-        defaultGateway = getDefaultGatewayAddress()
-        
-        if let defaultGateway = defaultGateway {
-            startPinging(address: defaultGateway)
+        guard let defaultGateway = getDefaultGatewayAddress() else { return }
+
+        initProcess(address: defaultGateway)
+        runProcess()
+
+        NotificationCenter.default.addObserver(forName: NSApplication.willTerminateNotification, object: nil, queue: .main) { [weak self] _ in
+            self?.process.terminate()
         }
     }
-    
-    deinit {
-        stopPinging()
+
+    func initProcess(address: String) {
+        process.executableURL = URL(fileURLWithPath: "/sbin/ping")
+        process.arguments = ["-i", "0.2", address]
+
+        let pipe = Pipe()
+        process.standardOutput = pipe
+
+        let outputHandle = pipe.fileHandleForReading
+        outputHandle.readabilityHandler = { [weak self] handle in
+            if let output = String(data: handle.availableData, encoding: .utf8), output.contains("timeout") {
+                guard let self = self else { return }
+
+                if self.failureCount >= 100 {
+                    Logger.main.error("Failed to ping 100 times. Terminating")
+                    self.process.terminate()
+                    DispatchQueue.main.async {
+                        self.running = false
+                    }
+                    return
+                }
+
+                self.failureCount += 1
+            }
+        }
     }
-    
-    func startPinging(address: String) {
-        Logger.main.debug("Starting ping")
-        
-        pingProcess = Process()
-        pingProcess?.executableURL = URL(fileURLWithPath: "/sbin/ping")
-        pingProcess?.arguments = ["-i", "0.2", address]
-        
+
+    func runProcess() {
         do {
-            try pingProcess?.run()
+            try process.run()
+            running = true
         } catch {
-            print("Error starting ping process: \(error.localizedDescription)")
+            Logger.main.error("Failed to run process: \(error.localizedDescription)")
         }
     }
-    
-    func stopPinging() {
-        Logger.main.debug("Stopping ping")
-        
-        pingProcess?.terminate()
-        pingProcess = nil
-    }
-    
+
     func getDefaultGatewayAddress() -> String? {
         Logger.main.debug("Getting default gateway")
-        
+
         let task = Process()
         let pipe = Pipe()
-        
+
         task.standardOutput = pipe
         task.standardError = pipe
         task.arguments = ["-c", "netstat -nr | awk '/default/ {print $2}' | head -n1"]
         task.launchPath = "/bin/zsh"
         task.launch()
-        
+
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
         let output = String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines)
-        
+
         task.waitUntilExit()
-        
+
         Logger.main.debug("Default gateway is: \(output ?? "nil")")
-        
+
         return output
     }
 }
--- TickFixer/TickFixerApp.swift
+++ TickFixer/TickFixerApp.swift
@@ -4,24 +4,19 @@ import SwiftUI
 @main
 struct TickFixerApp: App {
     @StateObject var pingManager = PingManager()
-    
+
     var body: some Scene {
         MenuBarExtra {
-            Button("TickFixer v1.0.0", action: {})
+            Button("TickFixer v1.0.0") {}
                 .disabled(true)
-            
+
             Divider()
-            
-            if !(pingManager.defaultGateway?.isEmpty ?? false) {
-                Button("Running...", systemImage: "figure.run", action: {})
-                    .labelStyle(.titleAndIcon)
-                    .disabled(true)
-            } else {
-                Button("Default Gateway Not Found", systemImage: "exclamationmark.triangle", action: {})
-                    .labelStyle(.titleAndIcon)
-                    .disabled(true)
-            }
-            
+
+            Button(pingManager.running ? "Running..." : "Not Running",
+                   systemImage: pingManager.running ? "figure.run" : "exclamationmark.triangle") {}
+                .labelStyle(.titleAndIcon)
+                .disabled(true)
+
             Button("Quit") {
                 Logger.main.debug("User initiated exit")
                 NSApplication.shared.terminate(nil)
